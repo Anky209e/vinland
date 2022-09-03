a939746fb26a62199cf300de86bf9deb2e58b9d1
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2022-09-03 16:18:25 +0530

    creted user model

--- user/models.py
+++ user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField("User", on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to="profile_images", null=True)
     previous_dets = models.TextField()
 
