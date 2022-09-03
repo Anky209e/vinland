79da6bf4c1c566d01f5ffb676ab2b963287cc64b
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2022-09-03 21:55:55 +0530

    changed the whole user app

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,7 @@
-from django.forms import ModelForm
-from .models import CustomUser
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-class RegisterForm(UserCreationForm):
-
+class SignupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password1']
-    
-
-    
+        fields = ('username', 'password1', 'password2',)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,23 @@
-from urllib import request
-from django.shortcuts import render
-from .forms import RegisterForm
-def register(request):
-    form = RegisterForm()
+import re
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from .forms import SignupForm
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm()
+
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.save()
+
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username= user.username, password = raw_password)
+            return redirect('/')
+    else:
+        form = SignupForm()
     return render(request, 'register.html', {'form':form})
